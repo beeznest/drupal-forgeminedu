e35c631acb148c38d6d62aafe9038995fccd4048
Agustin <agustin@softwinperu.(none)>
2015-01-21 15:13:52 -0500

    add script to user block

--- drupal/sites/all/themes/minedu/js/script.js
+++ drupal/sites/all/themes/minedu/js/script.js
@@ -6,11 +6,11 @@
         // Accordion effect to blocks in sidebar 
         $('.user-button-option').unbind('click');
         $('.user-button-option').click(function () {
-         /* $(this).addClass('accordion-active');
-          if (!(false == $(this).next().is(':visible'))) {
-            $(this).removeClass('accordion-active');
+          $(this).addClass('active');
+          if ((false == $('.user-option').is(':visible'))) {
+            $(this).removeClass('active');
           }
-          $(this).next().slideToggle(300);*/
+          $('.user-option').slideToggle(250);
         });
       });
     }  
