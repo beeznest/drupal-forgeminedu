f5db9115f2369de6f88f6b352e43d3f97fe8e041
Agustin <agustin@softwinperu.(none)>
2015-01-21 11:47:09 -0500

    add styles to header

--- drupal/sites/all/themes/minedu/js/script.js
+++ drupal/sites/all/themes/minedu/js/script.js
@@ -2,6 +2,16 @@
   Drupal.behaviors.minedu = {
     attach: function(context, settings) {
       $(document).ready(function() {
+
+        // Accordion effect to blocks in sidebar 
+        $('.user-button-option').unbind('click');
+        $('.user-button-option').click(function () {
+         /* $(this).addClass('accordion-active');
+          if (!(false == $(this).next().is(':visible'))) {
+            $(this).removeClass('accordion-active');
+          }
+          $(this).next().slideToggle(300);*/
+        });
       });
     }  
   }
