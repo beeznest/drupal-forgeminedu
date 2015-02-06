9adbf89378a41ab55446bf7883805125af8bef5f
danielsantisteban <danielsantistebanch@gmail.com>
2015-02-06 12:33:18 -0500

    improvement plugin view is added "chamilo courses" and add module menu attributes.

--- drupal/sites/all/modules/chamilo/chamilo-course-detail-ajax.tpl.php
+++ drupal/sites/all/modules/chamilo/chamilo-course-detail-ajax.tpl.php
@@ -1,34 +1,82 @@
 <div class="cb-inner-container-left"> 
   <ul> 
-    <li>
+    <li class="clearfix">
       <strong>Id:</strong>
-      <?php  ?>
+      <p>
+        <?php print $output->id; ?>
+      </p>
     </li>
-    <li>
+    <li class="clearfix">
       <strong>Costo:</strong>
-      <?php print $output->cost; ?>
+      <p>
+        <?php print $output->cost; ?>
+      </p>
     </li>
-    <li>
+    <li class="clearfix">
       <strong>Lugar:</strong>
-      <?php print $output->place; ?>
+      <p>
+        <?php print $output->place; ?>
+      </p>
     </li>
-    <li>
+    <li class="clearfix">
       <strong>Sesión permite visitantes:</strong>
-      <?php  ?>
+      <p>
+        <?php 
+          if (!empty($output->allow_visitors)) {
+            print $output->allow_visitors;
+          } 
+          else {
+            print 'No';
+          }
+        ?>
+      </p>
     </li>
-    <li>
+    <li class="clearfix">
       <strong>Cantidad de horas lectivas:</strong>
-      <?php print $output->duration; ?>
+      <p>
+        <?php print $output->duration; ?>
+      </p>
     </li>
-    <li>
+    <li class="clearfix">
       <img src="<?php print $output->banner; ?>" />
     </li>
   </ul> 
 </div>
 <div class="cb-inner-container-middle">
-  <p></p>
-  <a href="<?php print $output->brochure; ?>" donwload>Descargar brochure</a>
+  <p><?php print $output->as_description; ?></p>
+  <a href="<?php print $output->brochure; ?>" class="download-link" donwload>Descargar brochure</a>
 </div>
 <div class="cb-inner-container-right">
+  <?php if ($output->status == -1): ?>
+    <a href="<?php print $output->action_url; ?>" class="session-register">Inscribirse</a>
+  <?php endif; ?>
+
+  <?php if ($output->status == 0): ?>
+    <a href="<?php print $output->action_url; ?>" class="session-virtual-classroom">Ir aula virtual</a>
+  <?php endif; ?>
+
+  <?php if ($output->status == 1): ?>
+    <p class="session-message">
+      <?php print $output->message; ?>
+    </p>
+  <?php endif; ?>
+
+  <?php if ($output->status == 2): ?>
+    <p class="session-message">
+      <?php print $output->message; ?>
+    </p>
+  <?php endif; ?>
+
+  <?php if ($output->status == 3): ?>
+    <p class="session-message">
+      <?php print $output->message; ?>
+    </p>
+  <?php endif; ?>
+
+  <?php if ($output->status == 10): ?>
+    <p class="session-message">
+      <?php print $output->message; ?>
+    </p>
+  <?php endif; ?>
 </div>
 
