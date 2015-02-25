d355774d50628a2168356a3c1cdfde43c6bf0cbd
danielsantisteban <danielsantistebanch@gmail.com>
2015-02-25 15:46:10 -0500

    add class in tpl chamilo-search-course-page.

--- drupal/sites/all/modules/chamilo/chamilo-search-course-page.tpl.php
+++ drupal/sites/all/modules/chamilo/chamilo-search-course-page.tpl.php
@@ -1,36 +1,38 @@
-<ul>
-  <li>
-    <h4></h4>
-    <p></p>
-    <div class="session-mode f-left">
-      <em>Modalidad</em>
-      <p>Online</p>  
-    </div>
-    <div class="session-date-start f-left">
-      <em>Fecha de inicio</em>
-      <p>2015-03-02</p>  
-    </div>
-    <div class="session-date-end f-left">
-      <em>Fecha de fin</em>
-      <p>2016-04-30</p>
-    </div>
-    <div class="session-duration f-left">
-      <em>Duración</em>
-      <p>3</p>
-    </div>
-    <div class="session-vacancies f-left">
-      <em>Cupos</em>
-      <p>20</p>
-    </div>
-    <div class="session-schedule f-left">
-      <em>Horario</em>
-      <p>16:00 - 18:00</p>
-    </div>
-    <div class="ajax-container">
-      <div id="ajax-link-<?php ?>" class="ajax-link">
-        <a href="<?php print base_path(); ?>course-detail-ajax/nojs/<?php ?>" class="use-ajax">Ver más</a>
+<div class="chamilo-courses">
+  <ul>
+    <li class="chrow">
+      <h4></h4>
+      <p></p>
+      <div class="session-mode f-left">
+        <em>Modalidad</em>
+        <p>Online</p>  
       </div>
-      <div id="ajax-detail-<?php ?>" class="session-detail clearfix"></div>  
-    </div>
-  </li>
-</ul>
+      <div class="session-date-start f-left">
+        <em>Fecha de inicio</em>
+        <p>2015-03-02</p>  
+      </div>
+      <div class="session-date-end f-left">
+        <em>Fecha de fin</em>
+        <p>2016-04-30</p>
+      </div>
+      <div class="session-duration f-left">
+        <em>Duración</em>
+        <p>3</p>
+      </div>
+      <div class="session-vacancies f-left">
+        <em>Cupos</em>
+        <p>20</p>
+      </div>
+      <div class="session-schedule f-left">
+        <em>Horario</em>
+        <p>16:00 - 18:00</p>
+      </div>
+      <div class="ajax-container">
+        <div id="ajax-link-<?php ?>" class="ajax-link">
+          <a href="<?php print base_path(); ?>course-detail-ajax/nojs/<?php ?>" class="use-ajax">Ver más</a>
+        </div>
+        <div id="ajax-detail-<?php ?>" class="session-detail clearfix"></div>  
+      </div>
+    </li>
+  </ul>
+</div>
